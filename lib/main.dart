dfdebe1804860ce93a1a73d0c966f1a110cf8190
sherlockian4869 <k.yaeo@carep-tech.com>
2022-01-10 22:22:45 +0900

    relation firebase auth and cloudFirestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-void main() {
+void main() async {
+  // 初期化処理を追加
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -19,36 +25,105 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class SignInView extends StatelessWidget {
+class SignInView extends StatefulWidget {
   const SignInView({Key? key}) : super(key: key);
 
+  @override
+  _SignInViewState createState() => _SignInViewState();
+}
+
+class _SignInViewState extends State<SignInView> {
+  // メッセージ表示用
+  String infoText = '';
+  // 入力したメールアドレスとパスワード
+  String email = '';
+  String password = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(
-                onPressed: () async {
-                  await Navigator.of(context).pushReplacement(
-                    MaterialPageRoute(builder: (context) {
-                      return ChatView();
-                    })
-                  );
+        child: Container(
+          padding: EdgeInsets.all(24),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              // メールアドレス入力
+              TextFormField(
+                decoration: InputDecoration(labelText: 'メールアドレス'),
+                onChanged: (String value) {
+                  setState(() {
+                    email = value;
+                  });
                 },
-                child: const Text('ログイン')
-            )
-          ],
+              ),
+              //  パスワード入力
+              TextFormField(
+                decoration: InputDecoration(labelText: 'パスワード'),
+                onChanged: (String value) {
+                  setState(() {
+                    password = value;
+                  });
+                },
+              ),
+              Container(
+                padding: EdgeInsets.all(8),
+                child: Text(infoText),
+              ),
+              Container(
+                width: double.infinity,
+                child: ElevatedButton(
+                  child: const Text('ユーザ登録'),
+                  onPressed: () async {
+                    try {
+                      final FirebaseAuth auth = FirebaseAuth.instance;
+                      final result = await auth.createUserWithEmailAndPassword(email: email, password: password);
+
+                      await Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(builder: (context) {
+                          return ChatView(result.user!);
+                        })
+                      );
+                    } catch(e) {
+                      setState(() {
+                        infoText = '登録に失敗しました。';
+                      });
+                    }
+                  },
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                child: OutlinedButton(
+                  onPressed: () async {
+                    try {
+                      // メール・パスワードでログイン
+                      final FirebaseAuth auth = FirebaseAuth.instance;
+                      final result = await auth.signInWithEmailAndPassword(email: email, password: password);
+
+                      await Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(builder: (context) {
+                          return ChatView(result.user!);
+                        })
+                      );
+                    } catch(e) {
+                      infoText = '認証に失敗しました。';
+                    }
+                  },
+                  child: const Text('ログイン'),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
   }
 }
 
-class ChatView extends StatelessWidget {
-  const ChatView({Key? key}) : super(key: key);
 
+class ChatView extends StatelessWidget {
+  ChatView(this.user);
+  final User user;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -58,6 +133,7 @@ class ChatView extends StatelessWidget {
           IconButton(
             icon: Icon(Icons.close),
             onPressed: () async {
+              await FirebaseAuth.instance.signOut();
               await Navigator.of(context).pushReplacement(
                 MaterialPageRoute(builder: (context) {
                   return SignInView();
@@ -67,12 +143,56 @@ class ChatView extends StatelessWidget {
           )
         ],
       ),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(8),
+            child: Text('ログイン情報：${user.email}'),
+          ),
+          Expanded(
+              child: StreamBuilder<QuerySnapshot>(
+                stream: FirebaseFirestore.instance
+                .collection('posts')
+                .orderBy('date')
+                .snapshots(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    final List<DocumentSnapshot> documents = snapshot.data!.docs;
+                    return ListView(
+                      children: documents.map((document) {
+                        return Card(
+                          child: ListTile(
+                            title: Text(document['text']),
+                            subtitle: Text(document[('email')]),
+                            trailing: document['email'] == user.email
+                              ? IconButton(
+                                icon: const Icon(Icons.delete),
+                                onPressed: () async {
+                                  await FirebaseFirestore.instance
+                                        .collection('posts')
+                                        .doc(document.id)
+                                        .delete();
+                                },
+                              ) : null,
+                          ),
+                        );
+                      }).toList(),
+                    );
+                  }
+                  return const Center(
+                    child: Text('読込中...'),
+                  );
+                },
+              )
+          ),
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () async {
           await Navigator.of(context).push(
             MaterialPageRoute(builder: (context) {
-              return InputView();
+              return InputView(user);
             })
           );
         },
@@ -81,9 +201,16 @@ class ChatView extends StatelessWidget {
   }
 }
 
-class InputView extends StatelessWidget {
-  const InputView({Key? key}) : super(key: key);
 
+class InputView extends StatefulWidget {
+  InputView(this.user);
+  final User user;
+  @override
+  _InputViewState createState() => _InputViewState();
+}
+
+class _InputViewState extends State<InputView> {
+  String messageText = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -91,13 +218,46 @@ class InputView extends StatelessWidget {
         title: const Text('入力画面'),
       ),
       body: Center(
-        child: ElevatedButton(
-          child: const Text('戻る'),
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
+        child: Container(
+          padding: EdgeInsets.all(32),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextFormField(
+                decoration: InputDecoration(labelText: '投稿メッセージ'),
+                keyboardType: TextInputType.multiline,
+                maxLines: 3,
+                onChanged: (String value) {
+                  setState(() {
+                    messageText = value;
+                  });
+                },
+              ),
+              const SizedBox(height: 10),
+              Container(
+                width: double.infinity,
+                child: ElevatedButton(
+                  child: const Text('投稿'),
+                  onPressed: () async {
+                    final date = DateTime.now().toLocal().toIso8601String();
+                    final email = widget.user.email;
+                    await FirebaseFirestore.instance
+                        .collection('posts')
+                        .doc()
+                        .set({
+                          'text': messageText,
+                          'email': email,
+                          'date': date
+                    });
+                    Navigator.of(context).pop();
+                  },
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
   }
 }
+// test@test.com 123456
